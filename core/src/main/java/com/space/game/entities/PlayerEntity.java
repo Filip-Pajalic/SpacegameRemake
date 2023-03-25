a6ca9ce364675c729852661c513900791d4d0efa
filip pajalic <filip.pajalic@gmail.com>
2023-03-25 08:56:58 +0100

    added player

---
+++ core/src/main/java/com/space/game/entities/PlayerEntity.java
@@ -0,0 +1,11 @@
+package com.space.game.entities;
+
+import com.space.game.core.Entity;
+
+public class PlayerEntity extends Entity {
+
+
+    public PlayerEntity(String name) {
+        super(name);
+    }
+}
--- core/src/main/java/com/space/game/entities/ShipEntity.java
+++ core/src/main/java/com/space/game/entities/ShipEntity.java
@@ -1,42 +1,28 @@
 package com.space.game.entities;
 
-import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.math.Rectangle;
 import com.badlogic.gdx.math.Vector2;
-
-
-import com.space.game.components.CollisionComponent;
-import com.space.game.components.GraphicsCompoment;
-import com.space.game.components.InputComponent;
-import com.space.game.components.ParticleComponent;
-import com.space.game.components.PositionComponent;
-import com.space.game.components.PowerComponent;
-import com.space.game.components.ScoreComponent;
-import com.space.game.components.VelocityComponent;
+import com.space.game.components.*;
 import com.space.game.core.Entity;
 
 public class ShipEntity extends Entity {
 
     private float thrust, velocityMax;
     private int offsetHitbox;
-    private Vector2  dimension, spawnPosition;
+    private Vector2 dimension, spawnPosition;
 
     public ShipEntity(String name) {
         super(name);
 
-        this.dimension = new Vector2(32,32);
-        this.spawnPosition = new Vector2(100,10);
+        this.dimension = new Vector2(32, 32);
+        this.spawnPosition = new Vector2(100, 10);
         this.velocityMax = 80.0f;
         this.thrust = 120.0f;
         this.offsetHitbox = 0;
 
-        //Graphics
-        addComponent(new GraphicsCompoment());
-        getComponent(GraphicsCompoment.class).setTexture(new Texture("assets/ship1.png"));
-        getComponent(GraphicsCompoment.class).setSizeX((int)dimension.x);
-        getComponent(GraphicsCompoment.class).setSizeY((int)dimension.y);
+        addGraphicsComponents();
 
         //Position
         addComponent(new PositionComponent());
@@ -45,19 +31,15 @@ public class ShipEntity extends Entity {
         addComponent(new VelocityComponent());
 
         //Input
-        addComponent(new InputComponent());
-        getComponent(InputComponent.class).addInput(Input.Keys.W,new Vector2(0,1));
-        getComponent(InputComponent.class).addInput(Input.Keys.S,new Vector2(0,-1));
-        getComponent(InputComponent.class).addInput(Input.Keys.A,new Vector2(-1,0));
-        getComponent(InputComponent.class).addInput(Input.Keys.D,new Vector2(1,0));
-        getComponent(VelocityComponent.class).setActiveAcceleration(thrust);
+
+        addInputComponents();
 
         //Collision
         addComponent(new CollisionComponent());
-                getComponent(CollisionComponent.class).setShapeRect(new Rectangle(spawnPosition.x
-                ,spawnPosition.y,
-                32,
-                32));
+        getComponent(CollisionComponent.class).setShapeRect(new Rectangle(spawnPosition.x
+            , spawnPosition.y,
+            32,
+            32));
         getComponent(CollisionComponent.class).setOffset(offsetHitbox);
 
         //Score
@@ -73,5 +55,22 @@ public class ShipEntity extends Entity {
        /* addComponent(new ParticleComponent());
         getComponent(ParticleComponent.class).addEffect(Gdx.files.internal("assets/particle.red"), Gdx.files.internal("assets"));
         getComponent(ParticleComponent.class).getParticleEffectList().get(0).start();*/
+    }
+
+    private void addInputComponents() {
+        addComponent(new InputComponent());
+        getComponent(InputComponent.class).addInput(Input.Keys.W, new Vector2(0, 1));
+        getComponent(InputComponent.class).addInput(Input.Keys.S, new Vector2(0, -1));
+        getComponent(InputComponent.class).addInput(Input.Keys.A, new Vector2(-1, 0));
+        getComponent(InputComponent.class).addInput(Input.Keys.D, new Vector2(1, 0));
+        getComponent(VelocityComponent.class).setActiveAcceleration(thrust);
+    }
+
+    private void addGraphicsComponents() {
+        //Graphics
+        addComponent(new GraphicsCompoment());
+        getComponent(GraphicsCompoment.class).setTexture(new Texture("assets/ship1.png"));
+        getComponent(GraphicsCompoment.class).setSizeX((int) dimension.x);
+        getComponent(GraphicsCompoment.class).setSizeY((int) dimension.y);
     }
 }
